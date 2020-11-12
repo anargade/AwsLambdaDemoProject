ee0836dbc7d9dd00040d3eb7b018f67d7c62a2a6
anargade <nargadeashish35@gmail.com>
2020-11-12 15:17:14 +0530

    Added boto3 module

--- main.py
+++ main.py
@@ -1,18 +1,45 @@
 import string
 import sys
-# This is a sample Python script.
+import os
+import zipfile
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import boto3
 
 def print_hi(env, lname):
-    # Use a breakpoint in the code line below to debug your script.
-	print('Hi, There')
-	print('Arguments from the Jenkins Job: ' + str(sys.argv[1]), str(sys.argv[2]))
-	print('Method Parameters: ' + env +' '+lname)
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm','sample')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+	print('Hi, There')
+	# print('Arguments from the Jenkins Job: ' + str(sys.argv[1]), str(sys.argv[2]))
+	print('Method Parameters: ' + env +' '+lname)
+
+	folder_to_be_ziped = 'Lambdas'
+	with zipfile.ZipFile(lname+'.zip', 'w', zipfile.ZIP_DEFLATED) as lnamezip:
+		for dirpath, dirnames, files in os.walk(folder_to_be_ziped):
+			for file in files:
+				lnamezip.write(os.path.join(dirpath, file))
+s3_bucket_obj = boto3.resource('s3',aws_access_key_id='AKIASAATJPD4OY7M2E4X',aws_secret_access_key='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')
+for each_buk in s3_bucket_obj.buckets.all():
+	print("Bucket: "+each_buk.name)
+
+
+
+#s3 = boto3.client('s3')
+#s3.upload_file('lambda1.zip', 'mybucket', 'lambda1.zip')
+
+
+
+	#handle = zipfile.ZipFile('allpy.zip', 'w')
+
+	#for x in os.listdir():
+	#	if x.endswith('.py'):
+	#		handle.write(x, compress_type=zipfile.ZIP_DEFLATED)
+	#		handle.close()
+
+
+
+#if __name__ == '__main__':
+#    print_hi('dev','lambda1')
+
+
+
+
+
