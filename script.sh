ed2597975688c982d770d5c170e831f724fed3f7
anargade <62979186+anargade@users.noreply.github.com>
2020-11-16 02:04:51 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
  
 FUNCTION_NAME=$1
-
-zipfile="zip ${FUNCTION_NAME}.zip src/main/Lambdas/"
+echo "function_name: ${FUNCTION_NAME}"
+zipfile=zip ${FUNCTION_NAME}.zip src/main/Lambdas/
 echo "zip created with name: ${zipfile}"
 aws lambda create-function \
         --function-name "$FUNCTION_NAME" \
