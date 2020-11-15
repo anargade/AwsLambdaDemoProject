0d880957861f2d43110f5c73e5bfabaa416cc237
anargade <62979186+anargade@users.noreply.github.com>
2020-11-16 01:45:51 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ FUNCTION_NAME=$1
 aws lambda create-function \
         --function-name "$FUNCTION_NAME" \
         --runtime "python2.7" \
-        --zip-file "zip -r $FUNCTION_NAME.zip src/main/Lambdas/" \
+        --zip-file "fileb:// zip -r $FUNCTION_NAME.zip src/main/Lambdas/" \
         --handler "src/main/Lambdas/mylambda.my_handler" \
         --role "arn:aws:iam::137479420152:role/aws-lambda-demo-role" \
         --timeout 3 \
