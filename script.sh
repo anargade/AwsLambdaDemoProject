a3c03d6f8ec8539f79d04669849ce110b1bfcba5
anargade <62979186+anargade@users.noreply.github.com>
2020-11-16 02:31:09 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,6 +3,6 @@
 FUNCTION_NAME=$1
 echo "function_name: ${FUNCTION_NAME}"
 echo "$FUNCTION_NAME"
-zipfile=zip ${FUNCTION_NAME}.zip src/main/Lambdas/
+zipfile=zip -r ${FUNCTION_NAME}.zip ./Lambdas/
 echo "zip created with name: ${zipfile}"
 
