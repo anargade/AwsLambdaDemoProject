07dcc4b5e7676c3566e5f213169b61cb69f642f2
anargade <nargadeashish35@gmail.com>
2020-11-20 11:01:11 +0530

    Added Folder Structure

--- src/main/Lambdas/qa/qa_lambda.py
+++ src/main/Lambdas/qa/qa_lambda.py
@@ -4,5 +4,5 @@ def my_handler(event, context):
 
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps('Hello from qa_lambda!')
     }
